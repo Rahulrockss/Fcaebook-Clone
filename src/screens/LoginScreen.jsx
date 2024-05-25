58a88ff0f07f505f5886ca9c12f371ed52a37c08
Rahulrockss <rahulskumar343@gmail.com>
2024-05-25 17:09:37 +0530

    Login Component Created

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,12 +1,76 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import { View, Text, Image, StyleSheet, TextInput,TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import Vectoricon from '../utils/Vectoricon'
+import { Colors } from '../utils/Colors'
+import Logo from '../assests/Images/Logo.png'
 const LoginScreen = () => {
+    const[email,setEmail] = useState('')
+    const[Password,setPassword] = useState('')
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={style.container}>
+      <Vectoricon 
+        name="arrow-back-outline"
+        type="Ionicons"
+        color={Colors.black} 
+        size={20}/>
+        <View style ={style.subContainer}>   
+        <Image source={Logo} style={style.logoStyle} />
+            <TextInput style={style.inputBox} placeholder='Mobile number or email' 
+                value={email}  
+                onChangeText={value=> 
+                setEmail(value)
+                }/>
+            <TextInput style={style.inputBox} 
+                placeholder='Password' 
+                value={Password}  
+                onChangeText={value=> 
+                setPassword(value)
+                }/> 
+            <TouchableOpacity style={style.loginButton} >
+                <Text style={style.login}>Log in</Text>
+            </TouchableOpacity>                                                 
+        </View>       
     </View>
   )
 }
+const style = StyleSheet.create({
+   logoStyle: {
+    height:60,
+    width:60,
+    marginVertical:'20%',
 
-export default LoginScreen
+   },
+   container:{
+    padding:16,
+    
+   },
+   subContainer:{
+    justifyContent:'center',
+    alignItems:'center',
+   },
+   inputBox:{
+    borderWidth:1,
+    color:Colors.borderGrey,
+    padding:10,
+    borderRadius:12,
+    width:'95%',
+    marginTop:12,
+   },
+   loginButton:{
+    backgroundColor:Colors.primaryColor,
+    padding:10,
+    borderRadius:20,
+    width:'95%',
+    alignItems:'center',
+    marginTop:12,
+   },
+   login:{
+    color:Colors.white,
+    fontSize:15,
+    fontWeight:'500'
+
+   },
+
+})
+export default LoginScreen;
--- src/utils/Vectoricon.jsx
+++ src/utils/Vectoricon.jsx
@@ -1,7 +1,7 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import FontAwesome5 from 'react-native-vector-icons/dist/FontAwesome5';
+import {View} from 'react-native';
+import React from 'react';
 import FontAwesome from 'react-native-vector-icons/dist/FontAwesome';
+import FontAwesome5 from 'react-native-vector-icons/dist/FontAwesome5';
 import MaterialCommunityIcons from 'react-native-vector-icons/dist/MaterialCommunityIcons';
 import MaterialIcons from 'react-native-vector-icons/dist/MaterialIcons';
 import Feather from 'react-native-vector-icons/dist/Feather';
@@ -9,89 +9,48 @@ import AntDesign from 'react-native-vector-icons/dist/AntDesign';
 import Entypo from 'react-native-vector-icons/dist/Entypo';
 import Ionicons from 'react-native-vector-icons/dist/Ionicons';
 import EvilIcons from 'react-native-vector-icons/dist/EvilIcons';
-import Octions from 'react-native-vector-icons/dist/Octicons';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import Octicons from 'react-native-vector-icons/dist/Octicons';
+import Fontisto from 'react-native-vector-icons/dist/Fontisto';
 
-const Vectoricon = (props) => {
-    const {name,size,color,type,onPress} = props
+const VectorIcon = props => {
+  const {name, size, color, type, onPress, style} = props;
   return (
-    <View>
-    { type==="MaterialCommunityIcons"?(
-       <MaterialCommunityIcons 
-        onPress={onPress}
-        name={name}
-        size={size}
-        color={color}
-       /> 
-    )
-    : type==="FontAwesome" ?(
-       <FontAwesome 
-        onPress={onPress}
-        name={name}
-        size={size}
-        color={color}
-   /> )
-    : type==="FontAwesome5" ?(
-        <FontAwesome5 
-        onPress={onPress}
-        name={name}
-        size={size}
-        color={color}
-   />
-    ):type==="Feather" ?(
-        <Feather 
-        onPress={onPress}
-        name={name}
-        size={size}
-        color={color}
-   />
-    ):type==="AntDesign" ?(
-        <AntDesign 
-        onPress={onPress}
-        name={name}
-        size={size}
-        color={color}
-   />
-    ):type==="Entypo" ?(
-        <Entypo 
-        onPress={onPress}
-        name={name}
-        size={size}
-        color={color}
-   />
-    ):type==="Iconions" ?(
-        <Ionicons 
-        onPress={onPress}
-        name={name}
-        size={size}
-        color={color}
-   />
-    ):type==="EvilIcons" ?(
-        <EvilIcons 
-        onPress={onPress}
-        name={name}
-        size={size}
-        color={color}
-   />
-    ):
-    type==="Octions" ?(
-        <Octions 
-        onPress={onPress}
-        name={name}
-        size={size}
-        color={color}
-   />
-    ):
-    ( 
-    <MaterialIcons 
-        onPress={onPress}
-        name={name}
-        size={size}
-        color={color}
-   />)
-    }
+    <View style={style}>
+      {type === 'MaterialCommunityIcons' ? (
+        <MaterialCommunityIcons
+          onPress={onPress}
+          name={name}
+          size={size}
+          color={color}
+        />
+      ) : type === 'FontAwesome' ? (
+        <FontAwesome onPress={onPress} name={name} size={size} color={color} />
+      ) : type === 'FontAwesome5' ? (
+        <FontAwesome5 onPress={onPress} name={name} size={size} color={color} />
+      ) : type === 'Feather' ? (
+        <Feather onPress={onPress} name={name} size={size} color={color} />
+      ) : type === 'AntDesign' ? (
+        <AntDesign onPress={onPress} name={name} size={size} color={color} />
+      ) : type === 'Entypo' ? (
+        <Entypo onPress={onPress} name={name} size={size} color={color} />
+      ) : type === 'Ionicons' ? (
+        <Ionicons onPress={onPress} name={name} size={size} color={color} />
+      ) : type === 'EvilIcons' ? (
+        <EvilIcons onPress={onPress} name={name} size={size} color={color} />
+      ) : type === 'Octicons' ? (
+        <Octicons onPress={onPress} name={name} size={size} color={color} />
+      ) : type === 'Fontisto' ? (
+        <Fontisto onPress={onPress} name={name} size={size} color={color} />
+      ) : (
+        <MaterialIcons
+          onPress={onPress}
+          name={name}
+          size={size}
+          color={color}
+        />
+      )}
     </View>
-  )
-}
+  );
+};
 
-export default Vectoricon
+export default VectorIcon;
